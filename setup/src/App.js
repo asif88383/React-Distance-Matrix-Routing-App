d29a3d987dceb2462572a0e5e9c611bb1a377698
Muhammad Asif <asif88383@gmail.com>
2022-04-13 13:01:07 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import * as tt from '@tomtom-international/web-sdk-maps';
 
 const App = () => {
-  const [map, setMap] = useState({});
   const mapElement = useRef();
+  const [map, setMap] = useState({});
   const [marker, setMarker] = useState({});
   const [longitude, setLongitude] = useState(-0.112869);
   const [latitude, setLatitude] = useState(51.504);
@@ -13,15 +13,46 @@ const App = () => {
     let map = tt.map({
       key: process.env.REACT_APP_MAP_KEY,
       container:mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
       center: [longitude, latitude],
-      zoom: 13,
+      zoom: 14,
   })
   setMap(map);
-}, [])
+
+  const addMarker = () => {
+    const element = document.createElement('div');
+    element.className = 'marker';
+
+    const marker = new tt.Marker({
+      element: element,
+      draggable: true,
+    })
+  }
+  return () => map.remove();
+}, [longitude, latitude])
 
   return (
-    <div className="App">
-      <div ref={mapElement} className="map" ></div>
+    <div className='app'>
+      <div ref={mapElement} className="map" />
+
+      <div className="search-bar">
+        <h1>Where to?</h1>
+        <input
+          type="text"
+          placeholder="Enter longitude"
+          className='longitude'
+          onchange={(e) => {setLongitude(e.target.value)}}
+        />
+        <input
+          type="text"
+          placeholder="Enter latitude"
+          className='latitude'
+          onchange={(e) => {setLatitude(e.target.value)}}
+        />
+      </div>
     </div>
   );
 }
