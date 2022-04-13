2e149fd72c6a4dbc64e13676c44465b24872e7e5
Muhammad Asif <asif88383@gmail.com>
2022-04-13 12:34:55 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,19 +1,27 @@
-import {useEffect} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import './App.css';
+import * as tt from '@tomtom-international/web-sdk-maps';
 
 const App = () => {
+  const [map, setMap] = useState({});
+  const mapElement = useRef();
+  const [marker, setMarker] = useState({});
+  const [longitude, setLongitude] = useState(-0.112869);
+  const [latitude, setLatitude] = useState(51.504);
 
   useEffect(() => {
-    let map = new tt.map({
+    let map = tt.map({
       key: process.env.REACT_APP_MAP_KEY,
-      container:"mapElement",
+      container:mapElement.current,
+      center: [longitude, latitude],
+      zoom: 13,
   })
-
+  setMap(map);
 }, [])
 
   return (
     <div className="App">
-      
+      <div ref={mapElement} className="map" ></div>
     </div>
   );
 }
